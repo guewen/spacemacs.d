24b5cf5421fc88358a9b33cd92ffcc1a99de146d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2018-03-15 07:58:02 +0100

    Load local non-versioned config file
    for my custom projects commands

--- init.el
+++ init.el
@@ -329,6 +329,12 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  (let ((custom-local-file "~/.spacemacs.local.el"))
+   (when (file-exists-p custom-local-file)
+     (load-file custom-local-file)
+     )
+   )
+
   (spacemacs|define-custom-layout "Blog"
     :binding "b"
     :body
