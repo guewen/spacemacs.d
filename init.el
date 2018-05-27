6ed454ed1058e6c003f6dddba07220605a771a06
Guewen Baconnier <guewen@gmail.com>
2018-05-27 16:06:44 +0200

    Add rust

--- init.el
+++ init.el
@@ -36,6 +36,7 @@ values."
      yaml
      html
      python
+     rust
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
