92ffbf95c9307bfe0772e840b971ead755e3b06f
Guewen Baconnier <guewen.baconnier@qoqa.com>
2021-03-03 11:34:12 +0100

    Use gruvbox theme

--- init.el
+++ init.el
@@ -89,6 +89,7 @@ values."
                                       lsp-mode
                                       lsp-vue
                                       company-lsp
+                                      gruvbox-theme
                                       )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -166,7 +167,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(gruvbox-dark-soft
+                         spacemacs-dark
                          spacemacs-light)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
